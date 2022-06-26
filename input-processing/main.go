4826c55be76ce2bc2aaaad824022a655eebb4671
joshuadwinn <joshuadwinn14@gmail.com>
2022-06-25 22:50:44 -0400

    input-processing
    Kept it simple, but indicated what could be done to help handle larger loads from stdin

--- input-processing/main.go
+++ input-processing/main.go
@@ -2,16 +2,39 @@ package main
 
 import (
 	"bufio"
+	"io"
 	"fmt"
 	"os"
+	"log"
+	"strings"
 )
 
 func main() {
 	fmt.Println("SP// Backend Developer Test - Input Processing")
 	fmt.Println()
-
 	// Read STDIN into a new buffered reader
 	reader := bufio.NewReader(os.Stdin)
+	//Read data 1=MB at a time
+	bufferSize := make([]byte, 0, 1024)
+	for {
+		read, err := reader.Read(bufferSize[:cap(bufferSize)])
+		bufferSize = bufferSize[:read]
+		if read == 0 {
+			if err == nil {
+				continue
+			}
+			if err == io.EOF {
+				log.Fatal(err)
+				break
+			}
+		}
 
-	// TODO: Look for lines in the STDIN reader that contain "error" and output them.
+		content := string(bufferSize)
+
+		//In order to scale, this could be put into a separate go routine, or add a set of worker threads to parse
+		//the content using channels to store and pull from with infinite for loops and cancel contexts
+		if strings.Contains(content, "error") {
+			fmt.Println(content)
+		}
+	}
 }
